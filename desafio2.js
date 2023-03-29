e85e262f278a5fc95bd19230d94a24732a67d66f
Ramiro Lagrenade <ramirolagrenade@gmail.com>
2023-03-29 16:30:23 -0300

    Correcciónes

--- desafio2.js
+++ desafio2.js
@@ -1,18 +1,18 @@
 import fs from 'fs'
+import { title } from 'process'
 
 class ProductManager {
 
     constructor() {
-        this.path = './Product.json'
-        this.product = []
+        this.path = './products.json'
     }
 
 
     getProducts = async () => {
+
         if (fs.existsSync(this.path)) {
             const data = await fs.promises.readFile(this.path, 'utf-8')
             const products = JSON.parse(data)
-            // this.product = JSON.parse(data)
             return products
         }
         else {
@@ -20,24 +20,11 @@ class ProductManager {
         }
     }
 
-    getId = async () => {
-        if (fs.existsSync(this.path)) {
-            const data = await fs.promises.readFile(this.path, 'utf-8')
-            const products = JSON.parse(data)
-            // this.product = products
-            console.log(products.length)
-            return products.length
-        }
-        else {
-            return 0
-        }
-    }
-
     addProduct = async (title, description, price, thumbnail, code, stock) => {
 
-        this.product = await this.getProducts()
+        let products = await this.getProducts()
 
-        let idProducto = this.product.length
+        let idProducto = products.length
 
         let productos = {
             title: title,
@@ -46,28 +33,20 @@ class ProductManager {
             thumbnail: thumbnail,
             code: code,
             stock: stock,
-            id: ++idProducto
+            id: idProducto + 1
         }
 
-        if (idProducto === 1) {
-            // this.product.push(productos)
-            await fs.promises.writeFile(this.path, JSON.stringify(productos, null, '\t'))
-        }
-        else {
-            // this.product.push(productos)
-            await fs.promises.appendFile(this.path, JSON.stringify(productos, null, '\t'))
-        }
+        products.push(productos)
 
-        this.product.push(productos)
-
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'))
 
     }
 
 
     getProductById = async (IdProducto) => {
-        let contenido = await fs.promises.readFile(this.path, 'utf-8')
+        let products = await this.getProducts()
 
-        let proId = contenido.find(producto => producto.id === IdProducto)
+        let proId = products.find(product => product.id === IdProducto)
 
         if (proId) {
             return proId
@@ -82,77 +61,101 @@ class ProductManager {
 
     updateProduct = async (IdProducto, propiedad, update) => {
 
-        this.product = this.getProducts()
+        let product = await this.getProducts()
 
-        let producto = this.product.find(producto => producto.id === IdProducto)
+        let producto = product.find(producto => producto.id === IdProducto)
 
         if (propiedad === 'title') {
 
             producto.title = update
 
-            this.product.splice(IdProducto - 1, 1, producto)
+            product.splice(IdProducto - 1, 1, producto)
 
-            await fs.promises.writeFile(this.path, JSON.stringify(this.product, null, '\t'))
+            await fs.promises.writeFile(this.path, JSON.stringify(product, null, '\t'))
         }
 
         if (propiedad === 'description') {
 
             producto.description = update
 
-            this.product.splice(IdProducto - 1, 1, producto)
+            product.splice(IdProducto - 1, 1, producto)
 
-            await fs.promises.writeFile(this.path, JSON.stringify(this.product, null, '\t'))
+            await fs.promises.writeFile(this.path, JSON.stringify(product, null, '\t'))
         }
 
         if (propiedad === 'price') {
 
             producto.price = update
 
-            this.product.splice(IdProducto - 1, 1, producto)
+            product.splice(IdProducto - 1, 1, producto)
 
-            await fs.promises.writeFile(this.path, JSON.stringify(this.product, null, '\t'))
+            await fs.promises.writeFile(this.path, JSON.stringify(product, null, '\t'))
         }
 
         if (propiedad === 'thumbnail') {
 
             producto.thumbnail = update
 
-            this.product.splice(IdProducto - 1, 1, producto)
+            product.splice(IdProducto - 1, 1, producto)
 
-            await fs.promises.writeFile(this.path, JSON.stringify(this.product, null, '\t'))
+            await fs.promises.writeFile(this.path, JSON.stringify(product, null, '\t'))
         }
 
         if (propiedad === 'code') {
 
             producto.code = update
 
-            this.product.splice(IdProducto - 1, 1, producto)
+            product.splice(IdProducto - 1, 1, producto)
 
-            await fs.promises.writeFile(this.path, JSON.stringify(this.product, null, '\t'))
+            await fs.promises.writeFile(this.path, JSON.stringify(product, null, '\t'))
         }
 
         if (propiedad === 'stock') {
 
             producto.stock = update
 
-            this.product.splice(IdProducto - 1, 1, producto)
+            product.splice(IdProducto - 1, 1, producto)
 
-            await fs.promises.writeFile(this.path, JSON.stringify(this.product, null, '\t'))
+            await fs.promises.writeFile(this.path, JSON.stringify(product, null, '\t'))
         }
 
     }
 
 }
 
-const productManager = new ProductManager()
-
-productManager.addProduct('Play 5', 'Vamo a jugar', 2000, '-----', 123, 10)
-productManager.addProduct('Play 5', 'Vamo a jugar', 2000, '-----', 123, 10)
-productManager.addProduct('Play 5', 'Vamo a jugar', 2000, '-----', 123, 10)
-
-// console.log(productManager.getProducts())
-
-// productManager.deleteProduct()
 
 
 
+const productManager = new ProductManager('products.json')
+
+const test = async () => {
+
+    //Añadimos los productos al archivo json.
+
+    await productManager.addProduct('Play 5', 'Vamo a jugar', 2000, '-----', 123, 10)
+
+    await productManager.addProduct('Play 5', 'Vamo a jugar', 2000, '-----', 123, 10)
+
+    await productManager.addProduct('Play 5', 'Vamo a jugar', 2000, '-----', 123, 10)
+
+    //Visualizamos los productos subidos al archivo.
+
+    console.log(await productManager.getProducts())
+
+    // modificamos el contenido de algun producto espesifico por su id
+    
+    await productManager.updateProduct(3, 'title', 'wafle')
+    
+    await productManager.updateProduct(3, 'stock', 2)
+
+    // vemos un producto en concreto mediante su id
+    
+    console.log(await productManager.getProductById(3))
+    
+    // eliminamos el json que contiene los productos
+
+    // productManager.deleteProduct()
+
+}
+
+test()
